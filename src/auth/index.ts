61db7ac5a9449b37047af6da75f8ebcd372b81b6
Arthur Kok <arthurkok2@gmail.com>
2026-05-12 13:21:59 -0400

    feat: wire AuthProvider into app root

---
+++ src/auth/index.ts
@@ -0,0 +1,2 @@
+export { AuthProvider } from './AuthProvider';
+export { useAuth, type AuthState } from './authContext';
--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './auth';
 import { ControllerPage } from './pages/ControllerPage';
 import './styles.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/controller" element={<ControllerPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/controller" element={<ControllerPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
 );
 
